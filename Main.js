4c8a62522b776f6c8f7745aab9f170d5fc2e6193
Santiago <santi.gonzalez98@hotmail.com.ar>
2023-07-03 22:52:43 -0300

    Segúnda Entrega

--- Main.js
+++ Main.js
@@ -1,11 +1,9 @@
-// Arrow function como constructor de Autos
 const Auto = (modelo, color, precio) => ({
     modelo,
     color,
     precio
   });
   
-  // Array de autos
   let autos = [
     Auto("208 Active", "blanco", 10000),
     Auto("208 Active", "rojo", 11000),
@@ -17,7 +15,6 @@ const Auto = (modelo, color, precio) => ({
     Auto("408 Feline", "rojo", 17000),
   ];
   
-  // Función para mostrar el menú y obtener la opción seleccionada
   function mostrarMenu() {
     let menu = "Seleccione una opción:\n 0.Salir\n";
   
@@ -31,7 +28,6 @@ const Auto = (modelo, color, precio) => ({
     return opcionSeleccionada;
   }
   
-  // Función para filtrar autos por precio máximo
   function filtrarPorPrecioMaximo(precioMaximo) {
     const autosFiltrados = autos.filter(auto => auto.precio <= precioMaximo);
   
@@ -63,10 +59,8 @@ const Auto = (modelo, color, precio) => ({
         const formaPago = prompt("Seleccione la forma de pago:\n1. Contado\n2. Financiación");
   
         if (formaPago === "1") {
-          // Pago de contado
           alert(`El precio del auto seleccionado es: $${autoSeleccionado.precio}`);
         } else if (formaPago === "2") {
-          // Financiación
           alert(`El precio del auto seleccionado es: $${autoSeleccionado.precio}`);
   
           let montoFinanciado = parseFloat(prompt("Ingrese el monto a financiar:"));
@@ -78,8 +72,8 @@ const Auto = (modelo, color, precio) => ({
             mesesFinanciacion = parseInt(prompt("Ingrese la cantidad de meses de financiación:"));
           }
   
-          const tasaInteresAnual = 0.3; // Tasa de interés anual del 30%
-          const tasaInteresMensual = tasaInteresAnual / 12; // Tasa de interés mensual
+          const tasaInteresAnual = 0.3; 
+          const tasaInteresMensual = tasaInteresAnual / 12;
           const precioFinal = montoFinanciado * (1 + tasaInteresMensual * mesesFinanciacion);
           const valorCuota = precioFinal / mesesFinanciacion;
   
@@ -96,7 +90,7 @@ const Auto = (modelo, color, precio) => ({
         if (!isNaN(precioMaximoFiltrar) && precioMaximoFiltrar > 0) {
           filtrarPorPrecioMaximo(precioMaximoFiltrar);
         } else {
-          console.log("Precio máximo inválido. No se realizará el filtrado.");
+          console.log("Precio máximo inválido.");
         }
         break;
       default:
@@ -107,5 +101,5 @@ const Auto = (modelo, color, precio) => ({
     opcion = mostrarMenu();
   }
   
-  alert("Gracias por utilizar nuestro servicio. ¡Hasta luego!");
+  alert("Gracias por utilizar nuestro servicio de ventas en PEUGEOT. ¡Hasta luego!");
   
